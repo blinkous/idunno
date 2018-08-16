e19a4f4174d9be6748163d09acd6409765939243
blinkous <saradhani1@gmail.com>
2018-08-16 19:02:20 -0400

    Paused seekbar alert class testing to test changing the max of the seekbar.

--- app/src/main/java/com/example/idunno/TestingGradients.java
+++ app/src/main/java/com/example/idunno/TestingGradients.java
@@ -1,12 +1,17 @@
 package com.example.idunno;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class TestingGradients extends AppCompatActivity {
+
+    private int transition = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +51,7 @@ public class TestingGradients extends AppCompatActivity {
          * creates an rectangle gradient using the colors array
          * sets the imageView myRing's drawable to the gradient
          */
-        int[] colors = {
+        /*int[] colors = {
                 Color.rgb(255, 255, 255),   //white
                 Color.rgb(176, 66, 244),    //purple
                 Color.rgb(0, 157, 255),     //blue
@@ -55,7 +60,7 @@ public class TestingGradients extends AppCompatActivity {
         GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
         gradientDrawable.setShape(GradientDrawable.RECTANGLE);
         RelativeLayout layout = findViewById(R.id.activity_testing_gradients);
-        layout.setBackground(gradientDrawable);
+        layout.setBackground(gradientDrawable);*/
 
 //        ImageView image = (ImageView) findViewById(R.id.myRing);
 //        image.setImageDrawable(gradientDrawable);
@@ -68,6 +73,40 @@ public class TestingGradients extends AppCompatActivity {
         Drawable rect = ResourcesCompat.getDrawable(res, R.drawable.rect_blue_black_gradient, getTheme());
         ImageView iv = (ImageView) findViewById(R.id.myRing);
         iv.setBackground(rect);*/
+
+        ImageView image1 = (ImageView) findViewById(R.id.gradImage1);
+        Drawable drawable1 = image1.getDrawable();
+
+        ImageView image2 = (ImageView) findViewById(R.id.gradImage2);
+        Drawable drawable2 = image2.getDrawable();
+
+        ImageView image3 = (ImageView) findViewById(R.id.gradImage3);
+        Drawable drawable3 = image3.getDrawable();
+
+        int i = 0;
+        while(i++ < 100) {
+            //Using a transition drawable
+            switch (transition){
+                case 1:
+                    image2.setImageAlpha(0);
+                    image3.setImageAlpha(0);
+                    ((TransitionDrawable) drawable1).startTransition(1000);
+                    transition = 2;
+                    break;
+                case 2:
+                    image1.setImageAlpha(0);
+                    image3.setImageAlpha(0);
+                    ((TransitionDrawable) drawable2).startTransition(1000);
+                    transition = 3;
+                    break;
+                case 3:
+                    image2.setImageAlpha(0);
+                    image1.setImageAlpha(0);
+                    ((TransitionDrawable) drawable3).startTransition(1000);
+                    transition = 1;
+                    break;
+            }
+        }
     }
 
 }
